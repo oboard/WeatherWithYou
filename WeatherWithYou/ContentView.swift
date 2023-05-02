7472f4b8501e4074c89d6cd120613c5f826e7a9a
oboard <oboard@outlook.com>
2023-05-02 16:58:27 +0800

    Initial Commit

---
+++ WeatherWithYou/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeatherWithYou
+//
+//  Created by oboard on 2023/5/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherWithYou/WeatherWithYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWithYouApp.swift
+//  WeatherWithYou
+//
+//  Created by oboard on 2023/5/2.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWithYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
